632ec8de8a36debe5a34251647a579be70cadcf0
nabs-dev <nabihakashif631@gmail.com>
2025-07-29 19:10:41 +0500

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * DHL Clone - Logout Script
+ * 
+ * This file handles user logout functionality
+ */
+
+// Start session
+session_start();
+
+// Unset all session variables
+$_SESSION = [];
+
+// If it's desired to kill the session, also delete the session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+// Destroy the session
+session_destroy();
+
+// Redirect to login page
+header('Location: index.php');
+exit;
+?>
